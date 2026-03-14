0752fd1a60e415b5163888685e8f5c141dbbaa86
mkamoski <mkamoski@gmail.com>
2026-03-14 18:14:12 -0400

    Auto-ZIP on publish + add quick update script

---
+++ gui_runner/QUICK_UPDATE.ps1
@@ -0,0 +1,8 @@
+# MaxEnt GUI - Quick Rebuild & Commit
+# ONE-LINER for mkamoski1 - run from anywhere!
+
+# Build, ZIP, and stage for commit:
+cd C:\Users\mkamoski1\source\repos\maxent\gui_runner; .\Publish.ps1; cd ..\gui_runner_published; git add MaxEntRunner.zip
+
+# Then just commit and push:
+# git commit -m "Update GUI"; git push
